6c565ed451b92fee6b1b377940a467965ce54212
wangxuan <allen.wang@hrtps.com>
2024-02-05 19:52:57 +0800

    fix:icon

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import * as ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-// import App from './App.tsx'
 import "./index.css";
 import routers from "./router";
 
