d6e726c6557d32d4f48751f341b4c286f258cf6a
wangxuan <wangx1229@foxmail.com>
2024-02-06 18:04:31 +0800

    fix:404

--- src/pages/overview/index.tsx
+++ src/pages/overview/index.tsx
@@ -14,35 +14,35 @@ function OverView() {
     
 
       <Flex justify="space-between" gap={30} className="advantage">
-        <Card title="专业" style={{ width: "20%" }}>
+        <Card title="专业" style={{ width: "15vw" }}>
           <p>专业的教学团队，专业的授课体系</p>
           <p>专业的评估指标，专业的升学指导</p>
         </Card>
-        <Card title="高效" style={{ width: "20%" }} className="rotate8">
+        <Card title="高效" style={{ width: "15vw" }} className="rotate8">
           <p>高效的教学模式，高效的学习计划</p>
           <p>高效的时间管理，高效的服务理念</p>
         </Card>
-        <Card title="互动" style={{ width: "20%" }}>
+        <Card title="互动" style={{ width: "15vw" }}>
           <p>老师与学生的互动，学校与家长的互通</p>
           <p>学生与学生的互助，家长与家长的互联</p>
         </Card>
-        <Card title="共享" style={{ width: "20%" }} className="rotate-8">
+        <Card title="共享" style={{ width: "15vw" }} className="rotate-8">
           <p>共享丰富的资源，共享学习的心得</p>
           <p>共享成功的经验，共享收获的喜悦</p>
         </Card>
       </Flex>
 
       <Flex justify="space-between" gap={30} align="center" className="spacial">
-        <Card title="定制课程" style={{ width: "20%" }} className="rotate-8">
+        <Card title="定制课程" style={{ width: "15vw" }} className="rotate-8">
           ——高端VIP 一对一课程定制
         </Card>
-        <Card title="标准课程" style={{ width: "20%" }}>
+        <Card title="标准课程" style={{ width: "15vw" }}>
           ——TOEFL、IELTS、SAT多元化选择
         </Card>
-        <Card title="境外考试" style={{ width: "20%" }} className="rotate8">
+        <Card title="境外考试" style={{ width: "15vw" }} className="rotate8">
           ——资深顾问团队 全方位专业指导
         </Card>
-        <Card title="升学指导" style={{ width: "20%" }}>
+        <Card title="升学指导" style={{ width: "15vw" }}>
           ——专业领队 正规资质
         </Card>
       </Flex>
--- src/pages/team/index.tsx
+++ src/pages/team/index.tsx
@@ -11,140 +11,100 @@ import "./index.css";
 
 const { Meta } = Card;
 
+const memberInfo = [
+  {
+    name: '袁思聪-Yuen',
+    position: 'TOK联合创始人',
+    education: '英国纽卡斯尔大学硕士',
+    experience: '教龄7年',
+    src: Yuen,
+    key: 'Yuen',
+  },
+  {
+    name: '宋新宇-Song',
+    position: 'TOK联合创始人',
+    education: '上海理工大学硕士',
+    experience: '教龄8年',
+    src: Song,
+    key: 'Song',
+  },
+  {
+    name: '王雅馨-Jasmine',
+    position: 'TOK资深讲师',
+    education: '澳洲阿德莱德大学双学士',
+    experience: '教龄8年',
+    src: Jasmine,
+    key: 'Jasmine',
+  },
+  {
+    name: '郦梦鸽-Trista',
+    position: 'TOK资深讲师',
+    education: '美国明尼苏达大学双学士',
+    experience: '教龄6年',
+    src: Trista,
+    key: 'Trista',
+  },
+  {
+    name: '谭文菁-Carol',
+    position: 'TOK资深讲师',
+    education: '复旦大学学士',
+    experience: '教龄7年',
+    src: Carol,
+    key: 'Carol',
+  },
+  {
+    name: '陈文婷-Alice',
+    position: 'TOK资深讲师',
+    education: '美国密苏里大学硕士',
+    experience: '教龄9年',
+    src: Alice,
+    key: 'Alice',
+  },
+  {
+    name: '王欣-Katherine',
+    position: 'TOK资深讲师',
+    education: '英国阿斯顿大学学士',
+    experience: '教龄9年',
+    src: Katherine,
+    key: 'Katherine',
+  },
+  {
+    name: '陈碧倩-Annie',
+    position: 'TOK资深讲师',
+    education: '湖州师范大学大学',
+    experience: '教龄5年',
+    src: Annie,
+    key: 'Annie',
+  },
+
+]
+
 function Team() {
   return (
-    <Flex className="team" vertical gap={30}>
-      <Flex justify="space-between" gap={30} align="center" className="spacial">
-        <Card
-          hoverable
-          cover={<img alt="Yuen" src={Yuen} style={{ width: "18vw" }} />}
-        >
-          <Meta
-            title="袁思聪-Yuen"
-            description={
-              <>
-                <p>TOK联合创始人</p>
-                <p>英国纽卡斯尔大学硕士</p>
-                <p>教龄7年</p>
-              </>
-            }
-          />
-        </Card>
-
-        <Card
-          hoverable
-          cover={<img alt="Song" src={Song} style={{ width: "18vw" }} />}
-        >
-          <Meta
-            title="宋新宇-Song"
-            description={
-              <>
-                <p>TOK联合创始人</p>
-                <p>上海理工大学硕士</p>
-                <p>教龄8年</p>
-              </>
-            }
-          />
-        </Card>
-
-        <Card
-          hoverable
-          cover={<img style={{ width: "18vw" }} alt="Jasmine" src={Jasmine} />}
-        >
-          <Meta
-            title="王雅馨-Jasmine"
-            description={
-              <>
-                <p>TOK资深讲师</p>
-                <p>澳洲阿德莱德大学双学士</p>
-                <p>教龄6年</p>
-              </>
-            }
-          />
-        </Card>
-
-        <Card
-          hoverable
-          cover={<img style={{ width: "18vw" }} alt="Trista" src={Trista} />}
-        >
-          <Meta
-            title="郦梦鸽-Trista"
-            description={
-              <>
-                <p>TOK资深讲师</p>
-                <p>美国明尼苏达大学双学士</p>
-                <p>教龄6年</p>
-              </>
-            }
-          />
-        </Card>
-      </Flex>
-
-      <Flex justify="space-between" gap={30} align="center" className="spacial">
-        <Card
-          hoverable
-          cover={<img style={{ width: "18vw" }} alt="Carol" src={Carol} />}
-        >
-          <Meta
-            title="谭文菁-Carol"
-            description={
-              <>
-                <p>TOK资深讲师</p>
-                <p>复旦大学学士</p>
-                <p>教龄7年</p>
-              </>
-            }
-          />
-        </Card>
-
-        <Card
-          hoverable
-          cover={<img style={{ width: "18vw" }} alt="Alice" src={Alice} />}
-        >
-          <Meta
-            title="陈文婷-Alice"
-            description={
-              <>
-                <p>TOK资深讲师</p>
-                <p>美国密苏里大学硕士</p>
-                <p>教龄9年</p>
-              </>
-            }
-          />
-        </Card>
-
-        <Card
-          hoverable
-          cover={
-            <img style={{ width: "18vw" }} alt="Katherine" src={Katherine} />
-          }
-        >
-          <Meta
-            title="王欣-Katherine"
-            description={
-              <>
-                <p>TOK资深讲师</p>
-                <p>英国阿斯顿大学学士</p>
-                <p>教龄9年</p>
-              </>
-            }
-          />
-        </Card>
-        <Card
-          hoverable
-          cover={<img style={{ width: "18vw" }} alt="Annie" src={Annie} />}
-        >
-          <Meta
-            title="陈碧倩-Annie"
-            description={
-              <>
-                <p>TOK资深讲师</p>
-                <p>湖州师范大学大学</p>
-                <p>教龄5年</p>
-              </>
-            }
-          />
-        </Card>
+    <Flex className="team" vertical gap={16}>
+      <Flex justify="space-between" gap={16} align="flex-start" wrap="wrap">
+        {
+          memberInfo.map(member => (
+            <Card
+            key={member.key}
+            hoverable
+            style={{ width: "18vw" }}
+            cover={<img alt={member.key} src={member.src}  />}
+          >
+            <Meta
+              title={member.name}
+              description={
+                <>
+                  <p>{member.position}</p>
+                  <p>{member.education}</p>
+                  <p>{member.experience}</p>
+                </>
+              }
+            />
+          </Card>
+          ))
+        }
+       
       </Flex>
     </Flex>
   );
